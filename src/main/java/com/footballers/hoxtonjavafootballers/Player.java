274c6b23d1a14a2dce531319699c021d5d2385cc
ArbenitBytyqi <arbenitbytyqi7@gmail.com>
2022-10-06 22:19:40 +0200

    first

---
+++ src/main/java/com/footballers/hoxtonjavafootballers/Player.java
@@ -0,0 +1,53 @@
+package com.footballers.hoxtonjavafootballers;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Entity
+public class Player {
+    @Id
+    @GeneratedValue
+    public Integer id;
+    public String name;
+    public String nationality;
+    public Integer scoreOutOfTen;
+    public boolean isReplacement;
+
+    @ManyToOne
+    @JoinColumn(name = "teamId", nullable = true)
+    private Team team;
+
+    public Player() {
+    }
+}
+
+@RestController
+class PlayerController {
+    @Autowired
+    private PlayerRepo playerRepo;
+
+    @GetMapping("/players")
+    public List<Player> getAllPlayers() {
+        return playerRepo.findAll();
+    }
+
+    @PostMapping("/players")
+    public Player createPlayer(@RequestBody Player playerData) {
+        return playerRepo.save(playerData);
+    }
+}
+
+interface PlayerRepo extends JpaRepository<Player, Integer> {
+}
